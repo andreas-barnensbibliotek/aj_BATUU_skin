b0d36429191614e18d653dd8fc8ddf8af45d03ea
Andreas Josefsson <andreas@barnensbibliotek.se>
2019-09-25 08:28:38 +0200

    20190925 nav update 1

--- assets/js/eventhandlers/booklistEventhandler.js
+++ assets/js/eventhandlers/booklistEventhandler.js
@@ -130,64 +130,6 @@ const boklistEventHandler = () => {
 
 			return false;
 		});
-
-		// $mainboklistcontainer.on('click', '#aj_bb_btnStart', function(e) {
-		// 	return false;
-		// });
-
-		// $mainboklistcontainer.on('click', '.booklistHandler', function(e) {
-		// 	let $curBlObj = $(this);
-		// 	let $curblchkIcon = $curBlObj.find('div');
-		// 	let curbl = $(this).attr('data-boklistid');
-
-		// 	let bookid = $(this)
-		// 		.parent()
-		// 		.attr('data-currbookid');
-
-		// 	/// lägg till och tabort icon
-		// 	if ($curblchkIcon.hasClass('hidedrpchkimg')) {
-		// 		// avbocka vald boklista i Boklistningen
-		// 		$curblchkIcon.removeClass('hidedrpchkimg');
-		// 	} else {
-		// 		$curblchkIcon.addClass('hidedrpchkimg');
-		// 	}
-
-		// 	blobj.addtobooklist(curbl, bookid, function() {
-		// 		console.log('inlagd i boklista: ' + curbl + ' och bokid: ' + bookid);
-		// 	});
-
-		// 	/// kolla om yttre ikon skall ändras
-		// 	let $bookitemcontainer = $curBlObj.parent().parent();
-		// 	let Totalantalbooklists = $(this)
-		// 		.parent()
-		// 		.find('.booklistHandler');
-		// 	let Antalcheckedboklists = $bookitemcontainer.find('.hidedrpchkimg');
-
-		// 	let valdbtn = $bookitemcontainer.find('button');
-		// 	if (Totalantalbooklists.length == Antalcheckedboklists.length) {
-		// 		// avbocka boklistan
-		// 		valdbtn.addClass('btn-light').removeClass('btn-success');
-		// 		valdbtn
-		// 			.find('i')
-		// 			.removeClass('fa-check')
-		// 			.addClass('fa-ellipsis-v');
-		// 	} else {
-		// 		valdbtn.addClass('btn-success').removeClass('btn-light');
-		// 		valdbtn
-		// 			.find('i')
-		// 			.removeClass('fa-ellipsis-v')
-		// 			.addClass('fa-check');
-		// 	}
-
-		// 	return false;
-		// });
-
-		// $mainboklistcontainer.on('click', '.aj_bb_readsnow', function(e) {
-		// 	let bookid = $(this).attr('data-currbookid');
-		// 	blobj.laserjustnu(bookid, function() {
-		// 		console.log('laser nu: ' + bookid);
-		// 	});
-		// });
 	}
 	function jplistInitHandler() {
 		$mainboklistcontainer.jplist({
